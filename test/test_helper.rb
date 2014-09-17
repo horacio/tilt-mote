9b11f6435d7b939922a84e1c9fd2b8bb1c30ad5a
Horacio Bertorello <syrii@msn.com>
2014-09-17 20:26:16 -0300

    Add minimal test suite for Cuba plugin.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,24 @@
 require 'cutest'
+require 'cuba/test'
+require 'cuba/render'
 require File.expand_path("../lib/tilt/mote", File.dirname(__FILE__))
+require File.expand_path("../lib/cuba/mote", File.dirname(__FILE__))
+
+Cuba.plugin(Cuba::Render)
+Cuba.plugin(Mout::Render)
+
+Cuba.settings[:render][:views] = "./test/views"
+
+Cuba.define do
+  on "partial" do
+    res.write partial("dashboard")
+  end
+
+  on "view" do
+    res.write view("dashboard", title: "Test")
+  end
+
+  on "render" do
+    render("dashboard", title: "Test")
+  end
+end
--- test/tilt-mote_test.rb
+++ test/tilt-mote_test.rb
@@ -8,3 +8,37 @@ test 'renders a template' do
   assert result.strip == \
     "If you cannot do great things, do small things in a great way."
 end
+
+scope do
+  setup do
+    <<-HTML
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Test</title>
+  </head>
+  <body>
+    <p>Res, non verba.</p>\n
+  </body>
+</html>
+    HTML
+  end
+
+  test "partial renders a template without the default layout" do
+    get "/partial"
+
+    assert last_response.body["<p>Res, non verba.</p>"]
+  end
+
+  test "view renders a template with a default layout" do |tmpl|
+    get "/view"
+
+    assert last_response.body[tmpl]
+  end
+
+  test "render shortcut renders a template with a default layout" do |tmpl|
+    get "/render"
+
+    assert last_response.body[tmpl]
+  end
+end
